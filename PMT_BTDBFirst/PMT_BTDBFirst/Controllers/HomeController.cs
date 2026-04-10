20a38fc815aa675b2c693885f07b301f46bbd6d2
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-10 13:34:19 +0700

    PhanManhTung_Step4_UseScaffold

---
+++ PMT_BTDBFirst/PMT_BTDBFirst/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using Microsoft.AspNetCore.Mvc;
+using PMT_BTDBFirst.Models;
+using System.Diagnostics;
+
+namespace PMT_BTDBFirst.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ PMT_BTDBFirst/PMT_BTDBFirst/Models/DonHang.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PMT_BTDBFirst.Models;
+
+public partial class DonHang
+{
+    public int MaDh { get; set; }
+
+    public int? MaKh { get; set; }
+
+    public DateOnly? NgayDat { get; set; }
+
+    public virtual ICollection<ChiTietDonHang> ChiTietDonHangs { get; set; } = new List<ChiTietDonHang>();
+
+    public virtual KhachHang? MaKhNavigation { get; set; }
+}
