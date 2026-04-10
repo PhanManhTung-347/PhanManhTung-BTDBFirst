ee00d648f787134207d455b1e0ba2ea2ef5bdaaa
PhanManhTung-347 <phanmanhtung24112006@gmail.com>
2026-04-10 13:54:12 +0700

    PhanManhTung_Step7_AddLayout

---
+++ PMT_BTDBFirst/PMT_BTDBFirst/Models/KhachHang.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace PMT_BTDBFirst.Models;
+
+public partial class KhachHang
+{
+    public int MaKh { get; set; }
+
+    public string? TenKh { get; set; }
+
+    public virtual ICollection<DonHang> DonHangs { get; set; } = new List<DonHang>();
+}
---
+++ PMT_BTDBFirst/PMT_BTDBFirst/Models/QuanLyBanHangContext.cs
@@ -0,0 +1,100 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace PMT_BTDBFirst.Models;
+
+public partial class QuanLyBanHangContext : DbContext
+{
+    public QuanLyBanHangContext()
+    {
+    }
+
+    public QuanLyBanHangContext(DbContextOptions<QuanLyBanHangContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<ChiTietDonHang> ChiTietDonHangs { get; set; }
+
+    public virtual DbSet<DonHang> DonHangs { get; set; }
+
+    public virtual DbSet<KhachHang> KhachHangs { get; set; }
+
+    public virtual DbSet<SanPham> SanPhams { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=PHANMANHTUNG;Database=QuanLyBanHang;User Id=sa;Password=12345;TrustServerCertificate=True;");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<ChiTietDonHang>(entity =>
+        {
+            entity.HasKey(e => new { e.MaDh, e.MaSp }).HasName("PK__ChiTietD__F557D6E0B16B37D9");
+
+            entity.ToTable("ChiTietDonHang");
+
+            entity.Property(e => e.MaDh).HasColumnName("MaDH");
+            entity.Property(e => e.MaSp).HasColumnName("MaSP");
+
+            entity.HasOne(d => d.MaDhNavigation).WithMany(p => p.ChiTietDonHangs)
+                .HasForeignKey(d => d.MaDh)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__ChiTietDon__MaDH__3E52440B");
+
+            entity.HasOne(d => d.MaSpNavigation).WithMany(p => p.ChiTietDonHangs)
+                .HasForeignKey(d => d.MaSp)
+                .OnDelete(DeleteBehavior.ClientSetNull)
+                .HasConstraintName("FK__ChiTietDon__MaSP__3F466844");
+        });
+
+        modelBuilder.Entity<DonHang>(entity =>
+        {
+            entity.HasKey(e => e.MaDh).HasName("PK__DonHang__272586618C7CF80F");
+
+            entity.ToTable("DonHang");
+
+            entity.Property(e => e.MaDh)
+                .ValueGeneratedNever()
+                .HasColumnName("MaDH");
+            entity.Property(e => e.MaKh).HasColumnName("MaKH");
+
+            entity.HasOne(d => d.MaKhNavigation).WithMany(p => p.DonHangs)
+                .HasForeignKey(d => d.MaKh)
+                .HasConstraintName("FK__DonHang__MaKH__3B75D760");
+        });
+
+        modelBuilder.Entity<KhachHang>(entity =>
+        {
+            entity.HasKey(e => e.MaKh).HasName("PK__KhachHan__2725CF1E73EE38C2");
+
+            entity.ToTable("KhachHang");
+
+            entity.Property(e => e.MaKh)
+                .ValueGeneratedNever()
+                .HasColumnName("MaKH");
+            entity.Property(e => e.TenKh)
+                .HasMaxLength(100)
+                .HasColumnName("TenKH");
+        });
+
+        modelBuilder.Entity<SanPham>(entity =>
+        {
+            entity.HasKey(e => e.MaSp).HasName("PK__SanPham__2725081C8DE3F16D");
+
+            entity.ToTable("SanPham");
+
+            entity.Property(e => e.MaSp)
+                .ValueGeneratedNever()
+                .HasColumnName("MaSP");
+            entity.Property(e => e.TenSp)
+                .HasMaxLength(100)
+                .HasColumnName("TenSP");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
